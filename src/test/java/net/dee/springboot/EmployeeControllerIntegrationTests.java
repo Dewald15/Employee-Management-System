97c830ccd0822a6643cc8c3355f159eb7d1d4143
Dewald15 <vdbergdewald@gmail.com>
2024-06-12 14:25:16 +0200

    Implement integration test that performs a POST request to save an employee

---
+++ src/test/java/net/dee/springboot/EmployeeControllerIntegrationTests.java
@@ -0,0 +1,51 @@
+package net.dee.springboot;
+
+import net.dee.springboot.dto.EmployeeDto;
+import net.dee.springboot.repository.EmployeeRepository;
+import net.dee.springboot.service.EmployeeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class EmployeeControllerIntegrationTests {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+//    @BeforeEach
+//    public void before(){
+//        System.out.println("Before Each Test");
+//        employeeRepository.deleteAll().subscribe();
+//    }
+
+    @Test
+    public void testSaveEmployee(){
+        EmployeeDto employeeDto = new EmployeeDto();
+        employeeDto.setFirstName("John");
+        employeeDto.setLastName("Cena");
+        employeeDto.setEmail("john@gmail.com");
+
+        webTestClient.post().uri("/api/employees")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(employeeDto), EmployeeDto.class)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody()
+                .consumeWith(System.out::println)
+                .jsonPath("$.firstName").isEqualTo(employeeDto.getFirstName())
+                .jsonPath("$.lastName").isEqualTo(employeeDto.getLastName())
+                .jsonPath("$.email").isEqualTo(employeeDto.getEmail());
+    }
+}
